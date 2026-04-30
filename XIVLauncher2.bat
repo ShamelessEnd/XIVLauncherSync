2408e6d592a6d5fc6b074a6aacef707d5d13d71b
ShamelessEnd <shamelessend@hotmail.com>
2026-04-30 19:00:58 +1000

    %*

--- XIVLauncher2.bat
+++ XIVLauncher2.bat
@@ -2,5 +2,5 @@ cls
 @pushd %~dp0
 @echo off
 python SyncXIVLauncher.py 2
-python LaunchXIV.py 2
+python LaunchXIV.py 2 %*
 @popd
--- XIVLauncher3.bat
+++ XIVLauncher3.bat
@@ -2,5 +2,5 @@ cls
 @pushd %~dp0
 @echo off
 python SyncXIVLauncher.py 3
-python LaunchXIV.py 3
+python LaunchXIV.py 3 %*
 @popd
